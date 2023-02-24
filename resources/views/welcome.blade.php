e91686b405675259952e07076844499e003f6c33
juan9321 <juan.rocha5@etec.sp.gov.br>
2023-02-24 17:12:29 -0300

    rotas laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+       
+    </head>
+<h1> PAGINA 1 --- HOME </h1>
+<br><br><br>
+ <a href="produtos.php"><button>pagina 2</button></a>
+ <a href="empresa.php"><button>pagina 3</button></a>
+ <a href="contato.php"><button>pagina 4</button></a>
+    </body>
+</html>
